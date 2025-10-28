f298e4cf7afad5e321b3bef700028b3c62c4bb8b
rickNoise <emberix@gmail.com>
2025-10-28 17:49:40 +0800

    updated server main.go to Dial amqp

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	// where to connect to the RabbitMQ server
+	ampqURL := "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(ampqURL)
+	if err != nil {
+		fmt.Printf("failed to Dial amqp: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+	fmt.Println("RabbitMQ connection successful")
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+	sig := <-signalChan
+
+	fmt.Printf("\nReceived signal: %v\n", sig)
+	fmt.Println("Shutting down gracefully...")
 }
