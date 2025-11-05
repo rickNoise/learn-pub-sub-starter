ef049ff06ef01994ec840d32d7d50b2716d35d4f
rickNoise <emberix@gmail.com>
2025-11-05 15:36:58 +0700

    updated client to handle user REPL commands

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,20 +3,25 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 
-	gamelogic "github.com/rickNoise/learn-pub-sub-starter/internal/gamelogic"
-	pubsub "github.com/rickNoise/learn-pub-sub-starter/internal/pubsub"
-	routing "github.com/rickNoise/learn-pub-sub-starter/internal/routing"
+	"github.com/rickNoise/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/rickNoise/learn-pub-sub-starter/internal/pubsub"
+	"github.com/rickNoise/learn-pub-sub-starter/internal/routing"
 )
 
 func main() {
+	// CONSTANTS
+	unitValues := []string{"infantry", "cavalry", "artillery"}
+	locationValues := []string{"americas", "europe", "africa", "asia", "antarctica", "australia"}
+
 	fmt.Println("Starting Peril client...")
+	amqpURL := "amqp://guest:guest@localhost:5672/"
 
 	// connect to the RabbitMQ server
-	ampqURL := "amqp://guest:guest@localhost:5672/"
-	conn, err := amqp.Dial(ampqURL)
+	conn, err := amqp.Dial(amqpURL)
 	if err != nil {
 		fmt.Printf("failed to Dial amqp: %v\n", err)
 		os.Exit(1)
@@ -31,6 +36,7 @@ func main() {
 		username, err = gamelogic.ClientWelcome()
 	}
 
+	// declare and bind the pause queue
 	ch, qu, err := pubsub.DeclareAndBind(
 		conn,
 		routing.ExchangePerilDirect,
@@ -42,9 +48,73 @@ func main() {
 		fmt.Printf("error with DeclareAndBind: %v", err)
 		os.Exit(1)
 	}
+	fmt.Printf("Queue %v declared and bound!\n", qu.Name)
 
 	// remove once values are used
 	fmt.Println(ch, qu)
+
+	// create a new game state
+	gs := gamelogic.NewGameState(username)
+
+	// REPL user input loop
+	quitRequest := false
+	for !quitRequest {
+		// get user input
+		userInput := []string{}
+		for len(userInput) == 0 {
+			userInput = gamelogic.GetInput()
+		}
+
+		// logic based on input
+		switch userInput[0] {
+		case "spawn":
+			// expects e.g. spawn europe infantry
+			if len(userInput) != 3 {
+				fmt.Println("example usage: spawn europe infantry")
+				break
+			}
+			location := userInput[1]
+			unit := userInput[2]
+			if !slices.Contains(locationValues, location) || !slices.Contains(unitValues, unit) {
+				fmt.Println("example usage: spawn europe infantry")
+				fmt.Println("available locations:", locationValues)
+				fmt.Println("available unit types:", unitValues)
+				break
+			}
+			err := gs.CommandSpawn(userInput)
+			if err != nil {
+				fmt.Println("error spawning units:", err)
+			}
+		case "move":
+			// expects e.g. move europe 1
+			if len(userInput) != 3 {
+				fmt.Println("example usage: move europe 1")
+				break
+			}
+			location := userInput[1]
+			if !slices.Contains(locationValues, location) {
+				fmt.Println("example usage: move europe 1")
+				fmt.Println("available locations:", locationValues)
+				break
+			}
+			_, err := gs.CommandMove(userInput)
+			if err != nil {
+				fmt.Println(err)
+			}
+		case "status":
+			gs.CommandStatus()
+		case "help":
+			gamelogic.PrintClientHelp()
+		case "spam":
+			fmt.Println("Spamming not allowed yet!")
+		case "quit":
+			gamelogic.PrintQuit()
+			quitRequest = true
+		default:
+			fmt.Println("Invalid command!")
+		}
+
+	}
 
 	// keep client running
 	select {}
