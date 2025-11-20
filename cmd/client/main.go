be5be7d9125c9aaaa1ef8afba68a7a4dbe325847
rickNoise <emberix@gmail.com>
2025-11-20 12:43:31 +0700

    Cleaned up client main.go to align with solutions

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,163 +3,119 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"slices"
 
 	amqp "github.com/rabbitmq/amqp091-go"
-
 	"github.com/rickNoise/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/rickNoise/learn-pub-sub-starter/internal/pubsub"
 	"github.com/rickNoise/learn-pub-sub-starter/internal/routing"
 )
 
 func main() {
-	// CONSTANTS
-	unitValues := []string{"infantry", "cavalry", "artillery"}
-	locationValues := []string{"americas", "europe", "africa", "asia", "antarctica", "australia"}
-
 	fmt.Println("Starting Peril client...")
 
 	// ***************************** //
-	// **** CONNECT TO RABBITMQ **** //
+	// ** Connect to RabbitMQ ****** //
 	// ***************************** //
 
 	amqpURL := "amqp://guest:guest@localhost:5672/"
+
+	// Create connection
 	conn, err := amqp.Dial(amqpURL)
 	if err != nil {
 		log.Fatalf("failed to Dial amqp: %v\n", err)
 	}
 	defer conn.Close()
-	fmt.Println("Peril client successfully created connection to RabbitMQ!")
+	fmt.Println("Peril client successfully connected to RabbitMQ!")
+
+	// Create a channel for publishing messages on the connection
+	publishCh, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("could not create publish channel: %v", err)
+	}
+
+	// ***************************** //
+	// ** Initialise game state **** //
+	// ***************************** //
 
 	// Prompt user for a username
 	username, err := gamelogic.ClientWelcome()
 	for err != nil {
-		fmt.Println(err, "\npick a username:")
+		fmt.Println(err, "\npick a username: ")
 		username, err = gamelogic.ClientWelcome()
 	}
 
-	// Declare and bind the pause queue
-	chPause, quPause, err := pubsub.DeclareAndBind(
-		conn,
-		routing.ExchangePerilDirect,
-		routing.PauseKey+"."+username,
-		routing.PauseKey,
-		pubsub.QueueTransient,
-	)
-	if err != nil {
-		log.Fatalf("error with DeclareAndBind: %v", err)
-	}
-	fmt.Printf("Queue %v declared and bound!\n", quPause.Name)
-	defer chPause.Close()
-
-	// Declare and bind the queue receiving moves by other players
-	moveCh, moveQu, err := pubsub.DeclareAndBind(
-		conn,
-		routing.ExchangePerilTopic,
-		"army_moves."+username,
-		"army_moves.*",
-		pubsub.QueueTransient,
-	)
-	if err != nil {
-		fmt.Printf("error with DeclareAndBind: %v", err)
-		os.Exit(1)
-	}
-	fmt.Printf("Queue %v declared and bound!\n", moveQu.Name)
-	defer moveCh.Close()
-
 	// Create a new game state
 	gs := gamelogic.NewGameState(username)
+
+	// ******************************** //
+	// ** Subscribe to player queues ** //
+	// ******************************** //
 
 	// Subscribe to the pause queue
 	err = pubsub.SubscribeJSON(
 		conn,
 		routing.ExchangePerilDirect,
-		quPause.Name,
+		routing.PauseKey+"."+gs.GetUsername(),
 		routing.PauseKey,
 		pubsub.QueueTransient,
 		handlerPause(gs),
 	)
 	if err != nil {
-		log.Fatalf("could not subscribe to pause queue")
+		log.Fatalf("could not subscribe to pause queue: %v", err)
 	}
+	fmt.Println("Client subscribed to the pause queue!")
 
 	// Subscribe to the move queue
 	err = pubsub.SubscribeJSON(
 		conn,
 		routing.ExchangePerilTopic,
-		moveQu.Name,
-		"army_moves.*",
+		routing.ArmyMovesPrefix+"."+gs.GetUsername(),
+		routing.ArmyMovesPrefix+".*",
 		pubsub.QueueTransient,
-		handlerMove(gs),
+		handlerMove(gs, publishCh),
 	)
 	if err != nil {
-		log.Fatalf("could not subscribe to move queue")
+		log.Fatalf("could not subscribe to move queue: %v", err)
 	}
+	fmt.Println("Client subscribed to the move queue!")
 
-	// ***************************** //
-	// **** USER INPUT REPL ******** //
-	// ***************************** //
+	// ******************************** //
+	// ** User Input REPL ************* //
+	// ******************************** //
 
-	quitRequest := false
-	for !quitRequest {
-		// get user input
-		userInput := []string{}
-		for len(userInput) == 0 {
-			userInput = gamelogic.GetInput()
+	for {
+		userInput := gamelogic.GetInput()
+		if len(userInput) == 0 {
+			continue
 		}
 
-		// logic based on input
 		switch userInput[0] {
 		case "spawn":
 			// expects e.g. spawn europe infantry
-			if len(userInput) != 3 {
-				fmt.Println("example usage: spawn europe infantry")
-				break
-			}
-			location := userInput[1]
-			unit := userInput[2]
-			if !slices.Contains(locationValues, location) || !slices.Contains(unitValues, unit) {
-				fmt.Println("example usage: spawn europe infantry")
-				fmt.Println("available locations:", locationValues)
-				fmt.Println("available unit types:", unitValues)
-				break
-			}
-			err := gs.CommandSpawn(userInput)
+			err = gs.CommandSpawn(userInput)
 			if err != nil {
-				fmt.Println("error spawning units:", err)
+				fmt.Println(err)
+				continue
 			}
 		case "move":
 			// expects e.g. move europe 1
-			if len(userInput) != 3 {
-				fmt.Println("example usage: move europe 1")
-				break
-			}
-			location := userInput[1]
-			if !slices.Contains(locationValues, location) {
-				fmt.Println("example usage: move europe 1")
-				fmt.Println("available locations:", locationValues)
-				break
-			}
-			move, err := gs.CommandMove(userInput)
+			mv, err := gs.CommandMove(userInput)
 			if err != nil {
 				fmt.Println(err)
-			} else {
-				// publish the move
-				fmt.Println("sending a move message...")
-				// use PublishJSON function to publish a message to the exchange
-				err = pubsub.PublishJSON(
-					moveCh,
-					routing.ExchangePerilTopic,
-					"army_moves."+username,
-					move,
-				)
-				if err != nil {
-					fmt.Println("failed to publish message to the exchange:", err)
-					os.Exit(1)
-				}
-				fmt.Println("successfully published move!")
+				continue
 			}
+
+			err = pubsub.PublishJSON(
+				publishCh,
+				routing.ExchangePerilTopic,
+				routing.ArmyMovesPrefix+"."+mv.Player.Username,
+				mv,
+			)
+			if err != nil {
+				fmt.Printf("error: %s\n", err)
+				continue
+			}
+			fmt.Printf("Moved %v units to %s\n", len(mv.Units), mv.ToLocation)
 		case "status":
 			gs.CommandStatus()
 		case "help":
@@ -168,7 +124,7 @@ func main() {
 			fmt.Println("Spamming not allowed yet!")
 		case "quit":
 			gamelogic.PrintQuit()
-			quitRequest = true
+			return
 		default:
 			fmt.Println("Invalid command!")
 		}
