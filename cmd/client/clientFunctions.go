35fd07e65b66d7c97e9eae661e2f0ec50e4b0af2
rickNoise <emberix@gmail.com>
2025-11-20 13:40:13 +0700

    added a handlerWar for client

--- cmd/client/clientFunctions.go
+++ cmd/client/clientFunctions.go
@@ -51,3 +51,26 @@ func handlerMove(gs *gamelogic.GameState, publishCh *amqp.Channel) func(gamelogi
 		}
 	}
 }
+
+func handlerWar(gs *gamelogic.GameState) func(gamelogic.RecognitionOfWar) pubsub.Acktype {
+	return func(rw gamelogic.RecognitionOfWar) pubsub.Acktype {
+		defer fmt.Print("> ")
+
+		outcome, _, _ := gs.HandleWar(rw)
+		switch outcome {
+		case gamelogic.WarOutcomeNotInvolved:
+			// NackRequeue the message so another client can try to consume it
+			return pubsub.NackRequeue
+		case gamelogic.WarOutcomeNoUnits:
+			return pubsub.NackDiscard
+		case gamelogic.WarOutcomeOpponentWon:
+			return pubsub.Ack
+		case gamelogic.WarOutcomeYouWon:
+			return pubsub.Ack
+		case gamelogic.WarOutcomeDraw:
+			return pubsub.Ack
+		}
+		fmt.Println("error: unrecognised outcome from gs.HandleWar")
+		return pubsub.NackDiscard
+	}
+}
